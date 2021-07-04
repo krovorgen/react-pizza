0d1ec600c8cdc9b30e6ecb95428ff00d0521a267
Максим Сергеевич <krovorgen@gmail.com>
2021-07-05 00:55:29 +0300

    added Button

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import Button from '../Button';
+
+import logoSvg from '../../assets/img/pizza-logo.svg';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="container">
+                <div className="header__logo">
+                    <img width="38" src={logoSvg} alt="Pizza logo" />
+                    <div>
+                        <h1>React Pizza</h1>
+                        <p>самая вкусная пицца во вселенной</p>
+                    </div>
+                </div>
+                <div className="header__cart">
+                    <Button />
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
