6de65d8f7b6fd73348478ea08e1172a8e4f16698
Biarozka <maksim.biarozka@infourok.biz>
2021-07-05 14:32:43 +0300

    added ref

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,38 +2,38 @@ import React, { FC } from 'react';
 import classNames from 'classnames';
 
 type AppearancesType = {
-    outline?: boolean;
-    cart?: boolean;
-    add?: boolean;
-    circle?: boolean;
-    black?: boolean;
+  outline?: boolean;
+  cart?: boolean;
+  add?: boolean;
+  circle?: boolean;
+  black?: boolean;
 };
 
 interface IButtonProps {
-    onClick?: () => void;
-    addClass?: string;
-    appearance?: AppearancesType;
+  onClick?: () => void;
+  addClass?: string;
+  appearance?: AppearancesType;
 }
 
 const Button: FC<IButtonProps & AppearancesType> = ({
-    children,
-    addClass,
-    onClick,
-    ...appearance
+  children,
+  addClass,
+  onClick,
+  ...appearance
 }) => {
-    const classNamesList = {
-        'button--outline': appearance.outline,
-        'button--cart': appearance.cart,
-        'button--add': appearance.add,
-        'button--circle': appearance.circle,
-        'button--black': appearance.black,
-    };
+  const classNamesList = {
+    'button--outline': appearance.outline,
+    'button--cart': appearance.cart,
+    'button--add': appearance.add,
+    'button--circle': appearance.circle,
+    'button--black': appearance.black,
+  };
 
-    return (
-        <button onClick={onClick} className={classNames('button', addClass, classNamesList)}>
-            {children}
-        </button>
-    );
+  return (
+    <button onClick={onClick} className={classNames('button', addClass, classNamesList)}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -2,34 +2,34 @@ import React, { FC } from 'react';
 import { ActiveCategoryType } from '../../App';
 
 interface ICategoriesProps {
-    items: string[];
-    setActiveCategory: (index: ActiveCategoryType) => void;
-    activeCategory: ActiveCategoryType;
+  items: string[];
+  setActiveCategory: (index: ActiveCategoryType) => void;
+  activeCategory: ActiveCategoryType;
 }
 
 const Categories: FC<ICategoriesProps> = ({ items, setActiveCategory, activeCategory }) => {
-    return (
-        <div className="categories">
-            <ul>
-                <li
-                    className={activeCategory === null ? 'active' : ''}
-                    onClick={() => setActiveCategory(null)}
-                >
-                    Все
-                </li>
-                {items &&
-                    items.map((name, idx) => (
-                        <li
-                            onClick={() => setActiveCategory(idx)}
-                            className={activeCategory === idx ? 'active' : ''}
-                            key={name + idx}
-                        >
-                            {name}
-                        </li>
-                    ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div className="categories">
+      <ul>
+        <li
+          className={activeCategory === null ? 'active' : ''}
+          onClick={() => setActiveCategory(null)}
+        >
+          Все
+        </li>
+        {items &&
+          items.map((name, idx) => (
+            <li
+              onClick={() => setActiveCategory(idx)}
+              className={activeCategory === idx ? 'active' : ''}
+              key={name + idx}
+            >
+              {name}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Categories;
