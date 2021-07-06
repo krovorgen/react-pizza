d62d33dc49e7951ed854d9d9f8092d2f7e169212
Biarozka <maksim.biarozka@infourok.biz>
2021-07-06 17:20:30 +0300

    added axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route } from 'react-router-dom';
 
 import { Header } from './components';
 import { Cart, Home } from './pages';
 
+type ProductItemType = {
+  id: string;
+  imageUrl: string;
+  name: string;
+  types: number[];
+  sizes: number[];
+  price: number;
+  category: number;
+  rating: number;
+};
+
 const App = () => {
+  const [productItem, setProductItem] = useState<ProductItemType[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((response) => response.json())
+      .then((json) => {
+        setProductItem(json.pizzas);
+      });
+  }, []);
+
   return (
     <>
       <div className="wrapper">
