1c0dc2212e399657145686ecfdee5500717f9793
Максим Сергеевич <krovorgen@gmail.com>
2021-07-11 00:41:24 +0300

    optimized Categories

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,31 +1,24 @@
 import React, { FC, memo } from 'react';
 import { ICategoriesProps } from './types';
-import { ActiveCategoryIndexType } from '../../types';
 
-const Categories: FC<ICategoriesProps> = ({ categoriesMockData, onClickItem, activeCategory }) => {
-  const onSelectItem = (index: ActiveCategoryIndexType) => {
-    onClickItem(index);
-  };
-
-  return (
-    <div className="categories">
-      <ul>
-        <li className={activeCategory === null ? 'active' : ''} onClick={() => onSelectItem(null)}>
-          Все
-        </li>
-        {categoriesMockData &&
-          categoriesMockData.map((name, index) => (
-            <li
-              onClick={() => onSelectItem(index)}
-              className={activeCategory === index ? 'active' : ''}
-              key={name + index}
-            >
-              {name}
-            </li>
-          ))}
-      </ul>
-    </div>
-  );
-};
+const Categories: FC<ICategoriesProps> = ({ categoriesMockData, onClickItem, activeCategory }) => (
+  <div className="categories">
+    <ul>
+      <li className={activeCategory === null ? 'active' : ''} onClick={() => onClickItem(null)}>
+        Все
+      </li>
+      {categoriesMockData &&
+        categoriesMockData.map((name, index) => (
+          <li
+            onClick={() => onClickItem(index)}
+            className={activeCategory === index ? 'active' : ''}
+            key={name + index}
+          >
+            {name}
+          </li>
+        ))}
+    </ul>
+  </div>
+);
 
 export default memo(Categories);
